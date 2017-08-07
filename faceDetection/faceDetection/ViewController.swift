8245be0bec71b586056e3717752e8eabf23fb3a4
Yashi Pallav <puneetgupta5294@gmail.com>
2017-08-07 13:10:47 +0530

    project setup

---
+++ faceDetection/faceDetection/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  faceDetection
+//
+//  Created by Puneet on 07/08/17.
+//  Copyright © 2017 PuneetGupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
