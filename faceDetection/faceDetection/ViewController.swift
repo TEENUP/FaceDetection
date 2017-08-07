72e2479ed8003bcecf2c18f5ccbc19ad1f08bf43
Yashi Pallav <puneetgupta5294@gmail.com>
2017-08-07 13:23:00 +0530

    image view

--- faceDetection/faceDetection/ViewController.swift
+++ faceDetection/faceDetection/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
+        let image = UIImage(named: "sample1")
+        let imageView = UIImageView(image: image)
+        
+        view.addSubview(imageView)
         
     }
 
